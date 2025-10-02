199e6aec129916cbad4c1ab784a6d3fbc04b75e8
VitoriaMaglio <vitoriamaglio@gmail.com>
2025-10-02 11:38:01 -0300

    Operação de instanciação da base de dados.

---
+++ ddd-workshopmongo/src/main/java/com/estudando/dddworkshopmongo/config/Instantiation.java
@@ -0,0 +1,25 @@
+package com.estudando.dddworkshopmongo.config;
+
+import com.estudando.dddworkshopmongo.domain.User;
+import com.estudando.dddworkshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+@Configuration
+public class Instantiation implements CommandLineRunner {
+    //classe para instanciar objetos e salvar no banco
+    @Autowired
+    private UserRepository userRepository; //declarar injeção de dependência
+
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.deleteAll();
+        //declarar id nulo, pois é o banco que gera ele
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
