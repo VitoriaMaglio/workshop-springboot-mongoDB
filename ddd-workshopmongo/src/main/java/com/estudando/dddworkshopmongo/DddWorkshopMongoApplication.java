45149a9545f362786653026f101257560f7e7c52
VitoriaMaglio <vitoriamaglio@gmail.com>
2025-10-02 10:30:41 -0300

    Criando projeto com Spring.

---
+++ ddd-workshopmongo/src/main/java/com/estudando/dddworkshopmongo/DddWorkshopMongoApplication.java
@@ -0,0 +1,11 @@
+package com.estudando.dddworkshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddWorkshopMongoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DddWorkshopMongoApplication.class, args);
+    }
+}
